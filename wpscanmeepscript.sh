00ef853349e4c3bfe45680f255c6050ece1c9411
meepmaster360 <paulomotaborges@gmail.com>
2024-04-04 09:19:52 +0100

    1

--- wpscanmeepscript.sh
+++ wpscanmeepscript.sh
@@ -25,9 +25,9 @@ function app_install () {
 
 # Nmap installation	
 
-	if [ ! -x "$(command -v nmap)" ];then																	
-        echo -e "${RED}[+]${NOCOLOR} ${GREEN}Nmap not detected...Installing${NOCOLOR}"
-        sudo apt-get install nmap -y > installing;rm installing
+	if [ ! -x "$(command -v wpscan)" ];then																	
+        echo -e "${RED}[+]${NOCOLOR} ${GREEN}Wpscan not detected...Installing${NOCOLOR}"
+        sudo apt-get install wpscan -y > installing;rm installing
 		else
     	echo -e "\n${GREEN}[+]${NOCOLOR}Nmap detected"
 	fi
