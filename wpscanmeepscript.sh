8a0733a0aa9d3ff17729efad407d40517a1c1226
meepmaster360 <paulomotaborges@gmail.com>
2024-04-04 09:16:46 +0100

    1

--- wpscanmeepscript.sh
+++ wpscanmeepscript.sh
@@ -1,5 +1,80 @@
 #!/bin/bash
 
+# Author: meepmaster
+# Date: 06-05-2022
+# Description: Nmap scan
+
+
+# Color variables 
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+# Internet Connect
+
+function connect() {
+	ping -c 1 -w 3 google.com > /dev/null 2>&1																
+	if [ "$?" != 0 ];then
+		echo -e "\n${RED}[!]${NOCOLOR} ${GREEN}This script needs an active internet connection!${NOCOLOR}"
+		exit 1
+	fi
+}
+
+function app_install () {
+
+# Nmap installation	
+
+	if [ ! -x "$(command -v nmap)" ];then																	
+        echo -e "${RED}[+]${NOCOLOR} ${GREEN}Nmap not detected...Installing${NOCOLOR}"
+        sudo apt-get install nmap -y > installing;rm installing
+		else
+    	echo -e "\n${GREEN}[+]${NOCOLOR}Nmap detected"
+	fi
+}
+
+function menu () {
+    echo -e ""
+    echo -e "${GREEN}[?]${NOCOLOR} Scan type"
+    echo -e ""
+    echo -e "${GREEN}1${NOCOLOR} Fast Scan"
+    echo -e "${GREEN}2${NOCOLOR} Intense Scan"
+    echo -e ""
+    echo -e "${GREEN}3${NOCOLOR} Check/Install Dependencies"
+    echo -e "${GREEN}4${NOCOLOR} Help"
+    echo -e ""
+	echo -e "${GREEN}0${NOCOLOR} Exit/Quit"
+    echo -e ""
+    echo -e "${GREEN} Select one : ${NOCOLOR}\n"
+	read meno;
+    echo -e ""
+    
+    if [ $meno = 1 ]
+    then
+        nmap_ports_open_fast
+    elif [ $meno = 2 ]
+    then
+        nmap_ports_open_intense
+    elif [ $meno = 3 ]
+    then
+        app_install
+    elif [ $meno = 4 ]
+    then
+        help
+	elif [ $meno = 0 ]
+    then
+        banner
+		echo -e "\n${GREEN} Nice to meet you, Bye...${NOCOLOR}\n";sleep 2
+	exit		
+    else
+		banner
+		echo -e "\n${GREEN} Wrong option, Bye...${NOCOLOR}\n";sleep 2
+	exit
+    fi
+
+	menu
+}
+
 # Set the target WordPress URL
 TARGET_URL="http://example.com"
 
