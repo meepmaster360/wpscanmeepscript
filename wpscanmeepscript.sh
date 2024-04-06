ede2a5ad868597b5d57a866db99fc46c70bf03a9
meepmaster360 <paulomotaborges@gmail.com>
2024-04-06 09:23:02 +0100

    1

--- wpscanmeepscript.sh
+++ wpscanmeepscript.sh
@@ -45,7 +45,7 @@ function wpscan_install () {
 	if [ ! -x "$(command -v wpscan)" ];then																	
         echo -e "${RED}[+]${NOCOLOR} ${GREEN}Wpscan not detected...Installing${NOCOLOR}"
         sudo apt-get install wpscan -y
-		else
+	else
     	echo -e "\n${GREEN}[+]${NOCOLOR}WPscan detected"
 	fi
 }
@@ -150,29 +150,6 @@ function menu () {
 }  
 
 
-#### 
-
-# Prompt user for WordPress URL
-
-
-
-
-# Perform user enumeration
-
-function user_enumeration () {
-    echo "Performing user enumeration..."
-    wpscan --url "$website_url" --enumerate u > user_enum.txt
-    # Extract usernames from the user enumeration result
-    USERLIST=$(grep 'Username:' user_enum.txt | awk '{print $2}')
-} 
-
-
-
-# Perform brute force attack for each username
-for USERNAME in $USERLIST; do
-    echo "Performing brute force attack for user: $USERNAME..."
-    wpscan --url "$website_url" --passwords "$PASSWORDLIST" --user "$USERNAME" --max-threads 50 --disable-tls-checks --max-retries 3 --retry-connrefused --output "brute_force_result_$USERNAME.txt"
-done
 
 # Call Functions
 
